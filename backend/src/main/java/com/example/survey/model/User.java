bfff616371d28c7bf6957be33ea3736228fe600f
Dizon, S. <dizonsteven6810@gmail.com>
2026-03-01 15:55:20 +0800

    Established postresql and springboot connection with CRUD

---
+++ backend/src/main/java/com/example/survey/model/User.java
@@ -0,0 +1,28 @@
+package com.example.survey.model;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String email;
+
+    public User(){
+    }
+
+    public Long getId() {return id; }
+    public void setId(Long id) {this.id = id; }
+
+    public String getUsername() {return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getEmail() {return email; }
+    public void setEmail(String email) { this.email = email; }
+}
