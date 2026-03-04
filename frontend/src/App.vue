d7eb76c3de6e2fa4136e735e30b8a9301d8bd776
Dizon, S. <dizonsteven6810@gmail.com>
2026-03-04 21:30:32 +0800

    added frontend boilerplate

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,1 +1,17 @@
+<template>
+  <div class="app-container">
+  </div>
+</template>
 
+<script setup>
+
+</script>
+
+<style>
+
+.app-container {
+  min-height: 100vh;
+  width: 100%;
+  background-color: #ffffff;
+}
+</style>
