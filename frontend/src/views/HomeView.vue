8291a19c4a187cfe7e2e2f017037ad5237cebde1
Dizon, S. <dizonsteven6810@gmail.com>
2026-03-07 12:58:13 +0800

    fixed import bugs

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,9 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <Survey />
   </main>
 </template>
+
+<script setup>
+import Survey from '../components/Survey.vue';
+</script>
